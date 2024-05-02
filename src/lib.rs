6973f992c17c3f02a32970e05a4cb34678e94583
Thesecondbestname <nudelknoedel@gmail.com>
2024-05-02 18:05:23 +0200

    added closures, I'll try to migrate to unboxed ones, added wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-use std::{marker::PhantomData, mem};
-
 use pyo3::prelude::*;
 const BOARDSIZE: usize = 3;
 const PLAYERS: usize = 2;
-
+const PADDING: usize = BOARDSIZE * 3 + 2;
 struct Render;
 struct Update;
 struct Ready;
 struct Over;
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum Player {
     Circle,
     Cross,
@@ -18,29 +16,131 @@ enum Field {
     Empty,
     Player(Player),
 }
+#[derive(Copy, Clone, PartialEq, Eq)]
 struct Row([Field; BOARDSIZE]);
 impl Row {
+    fn new() -> Self {
+        Row([Field::Empty; BOARDSIZE])
+    }
     fn check_win(&self) -> bool {
-        self.0
-            .windows(2)
-            .scan(false, |s, i| {
-                if i[0] != i[1] {
-                    return None;
-                } else {
-                    Some(true)
-                }
-            })
-            .fold(false, |a, b| b && a)
+        let init = self.0[0];
+        self.0.iter().all(|a| a == &init)
     }
 }
-struct Game<State> {
+type Action = Box<(dyn Fn(Game) -> Game + Send + Sync + 'static)>;
+#[pyclass]
+struct State {
+    game: Game,
+    actions: Action,
+}
+impl State {
+    fn start() -> Self {
+        State {
+            game: Game::start(Player::Circle),
+            actions: Box::new(move |game| game),
+        }
+    }
+    fn then(self, f: fn(Game) -> Game) -> Self {
+        Self {
+            actions: Box::new(move |game| f((self.actions)(game))),
+            ..self
+        }
+    }
+    fn and(self, f: fn(Game) -> Game) -> Self {
+        Self {
+            actions: Box::new(move |game| f((self.actions)(game))),
+            ..self
+        }
+    }
+    fn end(self) -> () {
+        (self.actions)(self.game);
+    }
+    fn until(mut self, predicate: fn(&Game) -> bool) -> State {
+        fn rec(game: Game, predicate: fn(&Game) -> bool, a: &Action) -> Game {
+            match predicate(&game) {
+                true => return game,
+                false => rec((a)(game), predicate, a),
+            }
+        }
+        self.game = rec(self.game, predicate, &self.actions);
+        self
+    }
+}
+struct Game {
     board: [Row; BOARDSIZE],
     turn: Player,
-    s: PhantomData<State>,
+}
+impl Game {
+    fn start(p: Player) -> Self {
+        Game {
+            board: [Row::new(); BOARDSIZE],
+            turn: p,
+        }
+    }
+}
+fn print(game: Game) -> Game {
+    println!(
+        "{0}\n{1:-<width$}",
+        game.board
+            .iter()
+            .enumerate()
+            .fold(String::new(), |acc, (b, a)| format!(
+                "{acc}\n{:-<width$}\n{b}{a}",
+                "",
+                width = PADDING
+            )),
+        "",
+        width = PADDING,
+    );
+    game
+}
+fn try_update(mut game: Game, inp: String) -> Option<Game> {
+    let mut coords = inp.chars().skip(2);
+    let alpha = coords.next()?;
+    let num = coords.next()?;
+    let a = match alpha {
+        'a' => 1,
+        'b' => 2,
+        'c' => 3,
+        _ => return None,
+    };
+    let b = match num as usize {
+        0..=3 => num as usize,
+        _ => return None,
+    };
+    game.board[a].0[b] = Field::Player(game.turn.clone());
+    Some(game)
+}
+fn advance_turn(game: Game, player: Player) -> Game {
+    Game {
+        turn: player,
+        board: game.board,
+    }
+}
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn tic_tac_toe(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() -> () {
+        State::start().then(print).end();
+    }
 }
 impl_display!(Row, |s: &Row| {
     format!(
-        "{} |",
+        "{}|",
         s.0.iter()
             .fold(String::new(), |acc, a| format!("{acc}| {a}"))
     )
@@ -57,68 +157,6 @@ impl_display!(Player, |s: &Player| {
         Player::Cross => "X",
     }
 });
-impl Game<Render> {
-    fn print(self) -> Game<Update> {
-        println!(
-            "{2:-<0$}{1}",
-            BOARDSIZE * 3,
-            self.board
-                .iter()
-                .fold(String::new(), |acc, a| format!("{acc}\n{a}")),
-            ""
-        );
-        Game {
-            board: self.board,
-            turn: self.turn,
-            s: PhantomData,
-        }
-    }
-}
-struct Coordinate(usize, usize);
-impl Game<Update> {
-    fn try_update(mut self, inp: String) -> Option<Self> {
-        let mut coords = inp.chars().skip(2);
-        let alpha = coords.next()?;
-        let num = coords.next()?;
-        let a = match alpha {
-            'a' => 1,
-            'b' => 2,
-            'c' => 3,
-            _ => return None,
-        };
-        let b = match num as usize {
-            0..=3 => num as usize,
-            _ => return None,
-        };
-        self.board[a].0[b] = Field::Player(self.turn.clone());
-        Some(self)
-    }
-    fn advance_turn(self, player: Player) -> Game<Ready> {
-        Game {
-            turn: player,
-            board: self.board,
-            s: PhantomData,
-        }
-    }
-}
-// impl<Idc> Game<Idc> {
-//     fn check_win(self) -> Result<Game<Over>, Game<Ready>> {
-//         for row in self.board {}
-//     }
-// }
-
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
-#[pymodule]
-fn tic_tac_toe(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    Ok(())
-}
 #[macro_export]
 /// Takes a struct name as first argument and a closure of it's Struct
 /// Synopsys: (`struct_name`, |s: &`struct_name`| match s{...})
